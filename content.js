9c2bea2ec359a6d8d1e3d372e99dd04e0e94e846
Mohammed Talas <talas9@gmail.com>
2016-07-20 21:54:54 +0400

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,5 @@
 // content.js
-//console.log('Value before:' + its.detect.itunesDetected);
-//its.detect.itunesDetected=function(){return true;}
-//console.log('Value after:' + its.detect.itunesDetected);
 
-var elt = document.createElement("script");
-elt.innerHTML = "its.detect.itunesDetected=function(){return true;}its.detect.userOverrideSetItunesInstalled();";
-document.head.appendChild(elt);
+var e = document.createElement("script");
+e.innerHTML = "its.detect.itunesDetected=function(){return true;}its.detect.userOverrideSetItunesInstalled();";
+document.head.appendChild(e);
